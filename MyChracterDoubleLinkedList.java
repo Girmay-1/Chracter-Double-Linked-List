8be2ae2f5fd9527214901203c4f67cc42cc480ad
Girmay-1 <54249189+Girmay-1@users.noreply.github.com>
2021-06-06 22:05:26 -0500

    Update MyChracterDoubleLinkedList.java

--- MyChracterDoubleLinkedList.java
+++ MyChracterDoubleLinkedList.java
@@ -206,4 +206,15 @@ public class MyChracterDoubleLinkedList implements Iterable<Character>{
             this(data, null, null);
         }    
     }
+   public static void main(String[] args){
+        var list = new MyChracterDoubleLinkedList();
+        list.addFirst('A');
+        list.addFirst('B');
+        list.addFirst('C');
+        list.addFirst('D');
+        System.out.println(list);
+        list.printInReverse();
+        System.out.println(list.contains('E')); 
+        list.forEach(n -> System.out.println(n + " "));
+    }	
 }
